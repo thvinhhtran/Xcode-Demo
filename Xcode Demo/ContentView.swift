1cdbfb6ccd8bda603a8d549c6334e96799b24763
C3 <thvinhhtran29@gmail.com>
2024-10-25 17:18:22 +0700

    feat:building the card background

--- Xcode Demo/ContentView.swift
+++ Xcode Demo/ContentView.swift
@@ -9,16 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 20.0){
+            Image("niagarafalls")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+            
+            HStack {
+                
+                Text("Niagara Falls")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+                VStack{
+                    HStack {
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.leadinghalf.filled")
+                    }
+                    Text("(Reviews 361)")
+                }
+                .foregroundColor(.orange)
+                .font(.caption)
+              
+            }
+           
+            Text("Come visit the falls for an experience of a lifetime")
+            HStack{
+                Spacer()
+                Image(systemName: "fork.knife")
+                Image(systemName: "binoculars.fill")
+            }
+            .foregroundColor(.gray)
+            .font(.caption)
+            
         }
         .padding()
+        .background(Rectangle()
+            .foregroundColor(.white)
+            .cornerRadius(15)
+            .shadow(radius: 15))
+                    
+        .padding()
+
+
     }
 }
 
 #Preview {
     ContentView()
 }
+
