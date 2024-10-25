a5caa2a2a0e8dca1c3b43c5174db8ed7c4a46673
C3 <thvinhhtran29@gmail.com>
2024-10-25 13:22:52 +0700

    Initial Commit

---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Vinh on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
